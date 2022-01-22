dff35df9ea4931b16425d0a4c7d744ceb7e5f9aa
vanloc1808 <vanloc1808@gmail.com>
2022-01-22 16:14:15 +0700

    Python scripts

---
+++ Module01.Course IntroductionIntroToProgrammingAndThePythonLanguageVariablesConditionalsJupyterNotebookAndIDLE/sources/GreetUser.py
@@ -0,0 +1,9 @@
+"""
+This code will:
+    Prompt for your name
+    Set the 'name' variable
+    Print a message
+"""
+
+name = input("What is your name? ") # get the user's name
+print("Hello " + name) #print a message with the user's name
---
+++ Module01.Course IntroductionIntroToProgrammingAndThePythonLanguageVariablesConditionalsJupyterNotebookAndIDLE/sources/MyFirstScript.py
@@ -0,0 +1,10 @@
+print("one")
+print("two")
+print("three")
+print("four")
+print("five")
+
+# Here is a single line comment.
+"""
+Here is a multiple line comment.
+"""
