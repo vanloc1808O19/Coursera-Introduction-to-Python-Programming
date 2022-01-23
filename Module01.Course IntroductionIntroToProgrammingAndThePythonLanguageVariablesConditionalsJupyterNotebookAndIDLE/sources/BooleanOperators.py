d3fef9197e11f3da5f25811d1dd3e7fee8db9d9b
vanloc1808 <vanloc1808@gmail.com>
2022-01-23 10:49:21 +0700

    bool operators

---
+++ Module01.Course IntroductionIntroToProgrammingAndThePythonLanguageVariablesConditionalsJupyterNotebookAndIDLE/sources/BooleanOperators.py
@@ -0,0 +1,16 @@
+x = 5
+
+#AND operator
+print(x > 3 and x < 5) #False
+#The same as
+print(3 < x < 5) #False
+
+#OR operator
+y = 7
+print(y < 10 or y == 10) #True
+#The same as
+print(y <= 10) #True
+
+#NOT operator
+res = (y <= x)
+print(not res) #True
