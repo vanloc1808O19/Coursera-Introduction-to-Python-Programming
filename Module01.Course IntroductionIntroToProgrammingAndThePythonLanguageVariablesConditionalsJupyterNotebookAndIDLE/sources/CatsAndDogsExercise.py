dc3179afa81f8bd1ab8d1bac6374a97ffa178a63
vanloc1808 <vanloc1808@gmail.com>
2022-01-23 11:00:47 +0700

    cats & dogs exercise

---
+++ Module01.Course IntroductionIntroToProgrammingAndThePythonLanguageVariablesConditionalsJupyterNotebookAndIDLE/sources/CatsAndDogsExercise.py
@@ -0,0 +1,9 @@
+#set a variable x to "cats"
+x = "cats"
+
+#set a variable y to "dogs"
+y = "dogs"
+
+#reference the variables above, set a new variable s 
+s = "It's a raining " + x + " and " + y
+print(s)
