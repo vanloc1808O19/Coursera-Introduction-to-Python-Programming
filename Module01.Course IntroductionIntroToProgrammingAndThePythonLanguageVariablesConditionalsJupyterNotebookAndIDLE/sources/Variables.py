b0decddc4fe1c491648f2eb4a67afe7cbbca98c5
vanloc1808 <vanloc1808@gmail.com>
2022-01-23 10:55:26 +0700

    variables

--- Module01.Course IntroductionIntroToProgrammingAndThePythonLanguageVariablesConditionalsJupyterNotebookAndIDLE/sources/Variables.py
+++ Module01.Course IntroductionIntroToProgrammingAndThePythonLanguageVariablesConditionalsJupyterNotebookAndIDLE/sources/Variables.py
@@ -7,3 +7,22 @@ print(y)
 
 t = 'five'
 print(t)
+
+z = 2 * x + y
+print(z)
+
+z = z ** (y - 1)
+print(z)
+
+a = 18
+b = str(a)
+a *= 2 # a = a * 2
+b *= 2 # b = b * 2
+print(a)
+print(b)
+
+"""Combining variables"""
+myFavMovie = "Harry Potter"
+myFavActress = "Emma Watson"
+myFavs = "My favorite movie is " + myFavMovie + " and my favorite actress is " + myFavActress
+print(myFavs)
